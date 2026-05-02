595142081e3391b65db176e28e184f377bf09bd6
AyushDeuja <ayushdeuja11@gmail.com>
2026-05-02 20:44:30 +0545

    feat: add JWT token generation and auth DTOs

--- leave-management-api/obj/Debug/net10.0/leave-management-api.AssemblyInfo.cs
+++ leave-management-api/obj/Debug/net10.0/leave-management-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leave-management-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b938f65e98fb1c1ef83050a334b3f2c07fa0281")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed93ce0767bbcaef7f99a5ad44756e46b45b44db")]
 [assembly: System.Reflection.AssemblyProductAttribute("leave-management-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leave-management-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
