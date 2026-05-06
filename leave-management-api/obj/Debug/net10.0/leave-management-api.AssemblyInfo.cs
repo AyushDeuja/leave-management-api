38ee7332530fb4ed0b2e9d97245655133b50e9e9
AyushDeuja <ayushdeuja11@gmail.com>
2026-05-06 20:08:54 +0545

    feat: add leave types API with GET and POST endpoints

--- leave-management-api/obj/Debug/net10.0/leave-management-api.AssemblyInfo.cs
+++ leave-management-api/obj/Debug/net10.0/leave-management-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leave-management-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f01cbad221f525f5cd8a165642770d7e0e047a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e317ba66325a30871c169be0365277ff1553cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("leave-management-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leave-management-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
