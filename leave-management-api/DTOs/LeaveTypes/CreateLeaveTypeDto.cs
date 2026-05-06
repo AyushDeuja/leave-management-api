80e317ba66325a30871c169be0365277ff1553cc
AyushDeuja <ayushdeuja11@gmail.com>
2026-05-06 20:02:47 +0545

    leave type dto created

---
+++ leave-management-api/DTOs/LeaveTypes/CreateLeaveTypeDto.cs
@@ -0,0 +1,6 @@
+public class CreateLeaveTypeDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public int DefaultDaysPerYear { get; set; }
+}
---
+++ leave-management-api/DTOs/LeaveTypes/UpdateLeaveTypeDto.cs
@@ -0,0 +1,6 @@
+public class UpdateLeaveTypeDto
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public int? DefaultDaysPerYear { get; set; }
+}
